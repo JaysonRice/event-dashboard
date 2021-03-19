c01ab17d4c4cac6da331e823ec78566a31f80eb4
Jayson Rice <jayson.rice.95@gmail.com>
2021-03-19 12:39:11 -0400

    Project setup. Getting API data back from calendar range.

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-date-picker
+    v-model="searchRange"
+    @change="selectDate"
+    range
+    full-width
+    elevation="2"
+    prev-icon="mdi-skip-previous"
+    next-icon="mdi-skip-next"
+  ></v-date-picker>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      searchRange: [],
+    };
+  },
+  methods: {
+    ...mapActions(["getEventsForDateRange"]),
+
+    selectDate() {
+      const range = {
+        startDate: this.searchRange[0],
+        endDate: this.searchRange[1],
+      };
+      this.getEventsForDateRange(range);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,36 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const API_KEY = process.env.VUE_APP_SEATGEEK_KEY;
+
+const apiURL = (
+  type,
+  dateRange
+) => `https://api.seatgeek.com/2/events?taxonomies.name=${type}
+&client_id=${API_KEY}&venue.city=nashville&venue.state=TN&sort=score.desc&datetime_local
+.gte=${dateRange.startDate}&datetime_local.lte=${dateRange.endDate}`;
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    sporting: {},
+  },
+  mutations: {
+    setSportingEvents(state, payload) {
+      state.sporting = payload;
+    },
+  },
+  actions: {
+    async getEventsForDateRange({ dispatch }, range) {
+      dispatch("getSportingEvents", range);
+    },
+
+    async getSportingEvents({ commit }, range) {
+      const url = apiURL("sports", range);
+      const res = await fetch(url);
+      const data = await res.json();
+      commit("setSportingEvents", data);
+      console.log(data);
+    },
+  },
   modules: {},
 });
